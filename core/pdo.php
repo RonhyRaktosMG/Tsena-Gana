73ae05b5e43d95542acba922a906c034131b610a
RonhyRaktosMG <ronhyrakotondrafara08@gmail.com>
2026-05-05 21:48:07 +0300

    config environment

--- core/pdo.php
+++ core/pdo.php
@@ -1,7 +1,12 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=tsena_gana_db', 'tsena_gana_user', 'tsena_gana_password');
+    $pdo = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (\Throwable $th) {
